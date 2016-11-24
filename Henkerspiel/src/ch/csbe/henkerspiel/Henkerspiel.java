4603f2260aa9b6620b9b6ffc5c776df46717fe22
TheDiti <endrit_lena@hotmail.com>
2016-11-24 20:48:58 +0100

    Henkerspiel

--- Henkerspiel/src/ch/csbe/henkerspiel/Henkerspiel.java
+++ Henkerspiel/src/ch/csbe/henkerspiel/Henkerspiel.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-<<<<<<< HEAD
 /**
  * 
  * @author endrit.lena
@@ -23,29 +22,13 @@ public class Henkerspiel {
 	private static int anzrichtig = 0;	//erstellt einen integer namens anzrichtig mit dem wert 0
 	
 	//randomLetter wählt ein zufälliges Wort vom Enum Woerter
-=======
-public class Henkerspiel {
-	
-	private static List<Woerter> values = Arrays.asList(Woerter.values());
-	private static int size = values.size();
-	private static Random random = new Random();
-	private static char[] erraten;
-	public static String word;
-	public static String test;
-	private static int falsch = 0;
-	private static int anzrichtig = 0;
-	
->>>>>>> origin/master
 	public static String randomLetter(){
 		String a = values.get(random.nextInt(size)).toString();
 	    return a;
 	    
 	  }
-<<<<<<< HEAD
 	
 	//getErraten nimmt das zufällige wort
-=======
->>>>>>> origin/master
 	public static String getErraten(){
 		String tostring = "";
 		for(int i = 0;i<erraten.length;i++){
@@ -54,10 +37,7 @@ public class Henkerspiel {
 		return tostring;
 	}
 	
-<<<<<<< HEAD
 	//setErraten setzt das zufällige Wort
-=======
->>>>>>> origin/master
 	public static void setErraten(String er){
 		erraten = new char[er.length()];
 		for(int i =  0; i<er.length(); i++){
@@ -70,18 +50,11 @@ public class Henkerspiel {
 		}
 	}
 	
-<<<<<<< HEAD
 	//raten schaut ob der eingegebene Buchstabe mit einem buchstaben des Wortes identisch ist.
 	public static void raten(String eingabe) throws Exception{
 		boolean richtig = false;
 		for(int j = 0; j < erraten.length; j++){
 			if(word.charAt(j) == eingabe.charAt(0) || word.charAt(j) == eingabe.charAt(0)-32){ //wenn es identisch ist wird der buchstabe ausgegeben
-=======
-	public static void raten(String eingabe) throws Exception{
-		boolean richtig = false;
-		for(int j = 0; j < erraten.length; j++){
-			if(word.charAt(j) == eingabe.charAt(0) || word.charAt(j) == eingabe.charAt(0)-32){
->>>>>>> origin/master
 				erraten[j] = word.charAt(j);
 				richtig = true;
 				anzrichtig++;
@@ -90,11 +63,7 @@ public class Henkerspiel {
 				}
 			}
 		}
-<<<<<<< HEAD
 		if(!richtig){	//wenn es nicht identisch ist wird eine Linie des Strichmänchen gezeichnet
-=======
-		if(!richtig){
->>>>>>> origin/master
 			falsch++;
 			HenkerApp.maenchen();
 			if(falsch == 10){
@@ -103,23 +72,16 @@ public class Henkerspiel {
 		}
 	}
 
-<<<<<<< HEAD
 	//setFalsch wird ausgeführt wenn ein Buchstabe falsch ist
 	public static void setFalsch(int i){
 		falsch = i;
 	}
 	
 	//getFalsch gibt die anzahl der falsch eingegeben Buchstaben dem integer falsch zurück
-=======
-	public static void setFalsch(int i){
-		falsch = i;
-	}
->>>>>>> origin/master
 	public static int getFalsch(){
 		return falsch;
 	}
 
-<<<<<<< HEAD
 	//setAnzrichtig wird ausgeführt wenn ein Buchstabe richtig ist
 	public static void setAnzrichtig(int i){
 		anzrichtig = i;
@@ -131,11 +93,6 @@ public class Henkerspiel {
 	}
 	
 	//gew verlinkt zu gewonnen() im HenkerApp wenn man gewonnen hat
-=======
-	public static void ver() throws Exception{
-		HenkerApp.verloren(HenkerApp.stage);
-	}
->>>>>>> origin/master
 	public static void gew() throws Exception{
 		HenkerApp.gewonnen(HenkerApp.stage);
 }
