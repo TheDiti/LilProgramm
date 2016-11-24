1d5c2d1749086bd2906cfc775ddcfb3afbe6ad86
TheDiti <endrit_lena@hotmail.com>
2016-11-24 21:45:49 +0100

    Henkerspiel

---
+++ Henkerspiel/src/ch/csbe/henkerspiel/HenkerTest.java
@@ -0,0 +1,18 @@
+package ch.csbe.henkerspiel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HenkerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void testRaten(){
+		Henkerspiel hs = new Henkerspiel();
+		//TODO test
+	}
+
+}
